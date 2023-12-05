9fdc20ca098b5cca9713c18d568c0ba99d475d44
vanessa <20191230@uthh.edu.mx>
2023-12-05 12:01:13 -0600

    build configurado

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            output: 'public/build', // Especifica la carpeta de salida que deseas
         }),
     ],
 });
